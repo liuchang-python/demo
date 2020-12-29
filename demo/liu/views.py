cfeeb98d5153d861c36a38e5a88118c74af49e61
liuchang-python <2585408754@qq.com>
2020-12-29 22:13:31 +0800

    添加了第二个视图

--- demo/liu/views.py
+++ demo/liu/views.py
@@ -1,7 +1,12 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 
+
 # Create your views here.
 
 def index(request):
+    return HttpResponse('ok')
+
+
+def login(request):
     return HttpResponse('ok')
